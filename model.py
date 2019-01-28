52d25eb5910e8f13907c0eb36d7e031542d30523
GilesStrong <giles.strong@outlook.com>
2019-01-28 19:07:53 +0300

    test

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ You must supply at least 4 methods:
 - load: reloads the model.
 '''
 import pickle
-import numpy as np   # We recommend to use numpy arrays
+import numpy as np   # We recommend to use numpy arrays  
 from os.path import isfile
 from sklearn.linear_model import LogisticRegression
 
